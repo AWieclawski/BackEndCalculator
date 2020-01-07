19725e141b6c9262e502d2fb3c31012b160da5aa
AWieclawski <a_wieclawski@poczta.pl>
2020-01-07 22:33:58 +0100

    Add check op-by-op, no-by-no

--- src/CalculatorEngine.java
+++ src/CalculatorEngine.java
@@ -6,7 +6,12 @@ import java.util.Stack;
 public class CalculatorEngine {
 
 	final static String separator = "\\s";
-	final static String errValue = "Wrong value or operator";
+	final static String valueSeparator = ", ";
+	final static String errValue1 = "Found neither numeric, nor operator element";
+	final static String errValue2 = "There is no operator between values: ";
+	final static String errValue3 = "There is no value between operators: ";
+	final static String errValue4 = "Bracket not closed";
+	final static String errValue5 = "Found no numeric value in operation";
 	final static String errDivZero = "Cannot divide by zero";
 	final static String nonNumerics = "[^\\d.]";
 
@@ -29,8 +34,17 @@ public class CalculatorEngine {
 				values.push(elements[i]);
 
 			// If neither numeric nor operator recognized, returns error communicate
-			if (!checkBigDecimal(elements[i]) && !checkIfOperator(elements[i]))
-				return errValue;
+			if (!checkBigDecimal(elements[i]) && !checkOperatorOrValue(elements[i]))
+				return errValue1;
+
+			if (!checkIfLastElement(elements, i) && checkBigDecimal(elements[i]) && checkBigDecimal(elements[i + 1]))
+				return errValue2.concat(elements[i]).concat(valueSeparator)
+						.concat(elements[i + 1]); // error, if between values there is no
+			// operator
+
+			if (!checkIfLastElement(elements, i) && checkOperator(elements[i]) && checkOperator(elements[i + 1]))
+				return errValue3.concat(elements[i]).concat(valueSeparator)
+						.concat(elements[i + 1]); // error, if between operators there is no value
 
 			else if (checkSeparator(elements[i]))
 				continue;
@@ -49,10 +63,10 @@ public class CalculatorEngine {
 				operators.pop();
 
 				// finish operation waiting before bracket or brackets, if any
-				if (i == elements.length - 1) {	// check if current is the last operation
+				if (checkIfLastElement(elements, i)) { // check if current is the last operation
 					while (!operators.empty() && !checkBracket(operators.peek()))
 						values.push(workingOnStacks(operators.pop(), values.pop(), values.pop()));
-				} else {	// check if following operator has higher priority than current
+				} else { // check if following operator has higher priority than current
 					while (!operators.empty() && !checkBracket(operators.peek())
 							&& higherPriorityOp(elements[i + 1], operators.peek()))
 						values.push(workingOnStacks(operators.pop(), values.pop(), values.pop()));
@@ -122,7 +136,7 @@ public class CalculatorEngine {
 		return 0;
 	}
 
-	private static boolean checkIfOperator(String testedElement) {
+	private static boolean checkOperatorOrValue(String testedElement) {
 		if (checkOperator(testedElement) || checkBracket(testedElement))
 			return true;
 		else
@@ -131,6 +145,13 @@ public class CalculatorEngine {
 
 	private static boolean checkSeparator(String testedElement) {
 		if (testedElement.equals(separator))
+			return true;
+		else
+			return false;
+	}
+
+	private static boolean checkIfLastElement(String[] elements, int i) {
+		if (i == elements.length - 1)
 			return true;
 		else
 			return false;
@@ -156,11 +177,11 @@ public class CalculatorEngine {
 		if (checkBigDecimal(a)) {
 			aBD = stringToBD(a);
 		} else
-			return errValue;
+			return errValue5;
 		if (checkBigDecimal(b)) {
 			bBD = stringToBD(b);
 		} else
-			return errValue;
+			return errValue5;
 
 		switch (op) {
 		case "+":
@@ -189,8 +210,8 @@ public class CalculatorEngine {
 		tests.add("127"); // 127
 		tests.add("-127"); // -127
 		tests.add("2 + 3"); // 5
-		tests.add("2 + 3 +  4"); // 9 - double spaces removed
-		tests.add("  7  + 3 -   4"); // 6 - initial and more multispaces removed
+		tests.add("2 + 3 +  4"); // 9
+		tests.add("  7  + 3 -   4"); // 6
 		tests.add("2 + 3 + 4 + 5"); // 14
 		tests.add("2 - 6"); // -4
 		tests.add("2 - 3 - 4"); // -5
@@ -201,9 +222,9 @@ public class CalculatorEngine {
 		tests.add("10 * 5 / 0"); // "Cannot divide by zero"
 		tests.add("22 * 10 / ( 5 - ( 2 + 3 ) )"); // "Cannot divide by zero"
 		tests.add("2 / 2 + 3 * 4"); // 13
-		tests.add("5 / ( 2 + 3 ) * ( 5 - 1 )"); // 4 - more brackets
-		tests.add("1 + 20 / ( ( 2 + 3 ) * ( 4 - 2 ) )"); // 3 - multilevel brackets
-		tests.add("20 / ( ( 2 + 3 ) * ( 4 - 2 ) ^ 3 )"); // 0.5 - multilevel brackets
+		tests.add("5 / ( 2 + 3 ) * ( 5 - 1 )"); // 4
+		tests.add("1 + 20 / ( ( 2 + 3 ) * ( 4 - 2 ) )"); // 3
+		tests.add("20 / ( ( 2 + 3 ) * ( 4 - 2 ) ^ 3 )"); // 0.5
 		tests.add("7.7 - 3.3 - 4.4"); // 0
 		tests.add("8.8 - 3.3 - .4"); // 5.1
 		tests.add("100 * ( 2 + 12 )"); // 1400
@@ -212,9 +233,11 @@ public class CalculatorEngine {
 		tests.add("1.652335819E9"); // 1652335819
 		tests.add("-2.4323387584534e11"); // -243233875845.34
 		tests.add("-4.4323387584534exp9"); // Wrong value
-		tests.add("12 + 3oo - 5"); // Wrong value
-		tests.add("3.5 - 1,3 - .4"); // Wrong value
-		tests.add("1 # 2 - .4"); // Wrong value
+		tests.add("12 + 3oo - 5"); // Found neither numeric, nor operator element
+		tests.add("3.5 - 1,3 - .4"); // Found neither numeric, nor operator element
+		tests.add("1 # 2 - .4"); // Found neither numeric, nor operator element
+		tests.add("1 * * 2 - .4"); // There is no value between operators
+		tests.add("-10  2.5 - 1.1 )"); // There is no operator between values : 
 		tests.add(" "); // Wrong value
 		tests.add(""); // Wrong value
 
